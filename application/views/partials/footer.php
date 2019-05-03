26e4783bcb60f6323d7fdfa42ac9704864c043f6
Inant Kharisma <inant.kharisma@gmail.com>
2019-05-03 08:49:32 +0700

    ci sama template nya

---
+++ application/views/partials/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+          <div class="container-fluid clearfix">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
+              <a href="http://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with
+              <i class="mdi mdi-heart text-danger"></i>
+            </span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <!-- plugins:js -->
+  <script src="<?= base_url().'assets/vendors/js/vendor.bundle.base.js'?>"></script>
+  <script src="<?= base_url().'assets/vendors/js/vendor.bundle.addons.js' ?>"></script>
+  <!-- endinject -->
+  <!-- inject:js -->
+  <script src="<?= base_url().'assets/js/off-canvas.js' ?>"></script>
+  <script src="<?= base_url().'assets/js/misc.js' ?>"></script>
+  <!-- endinject -->
+  <script src="<?= base_url().'assets/jquery/jquery-3.4.1.js' ?>"></script>
+
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <!-- End custom js for this page-->
+</body>
+
+</html>
