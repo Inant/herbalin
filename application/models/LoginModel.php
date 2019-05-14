5f373ce367c726aa3041718ccc8a5a410f359da2
Inant Kharisma <inant.kharisma@gmail.com>
2019-05-14 09:23:27 +0700

    revert

--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -3,18 +3,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class LoginModel extends CI_Model
 {
-    public function login($username, $password)
+    public function cekLogin($where)
     {
-        // fetch by username first
-        $this->db->where('username', $username);
-        $query = $this->db->get('user');
-        $result = $query->row_array(); // get the row first
-
-        if (!empty($result) && password_verify($password, $result['password'])) {
-            // if this username exists, and the input password is verified using password_verify
-            return $result;
-        } else {
-            return false;
-        }
+        return $this->db->get_where('user', $where);
     }
 }
